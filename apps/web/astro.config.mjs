7d7c9ff2b535d8cbda6e0869fbda38268a86deb3
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2025-03-14 22:55:39 +0530

    chore: :broom: update astro

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -42,9 +42,6 @@ export default defineConfig({
     },
   },
   scopedStyleStrategy: "where",
-  experimental: {
-    contentLayer: true,
-  },
   output: "server",
   adapter: cloudflare({
     platformProxy: {

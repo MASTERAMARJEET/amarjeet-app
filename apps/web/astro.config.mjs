da28daede3f31b1469ac4dc4461150666adefd48
Amarjeet Kumar <masteramarjeet@users.noreply.github.com>
2025-03-15 03:11:10 +0530

    chore: :broom: update tailwind

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -2,7 +2,7 @@ import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
@@ -12,9 +12,6 @@ import { SITE } from "./src/config";
 export default defineConfig({
   site: SITE.website,
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     react(),
     sitemap(),
     mdx(),
@@ -37,6 +34,7 @@ export default defineConfig({
     },
   },
   vite: {
+    plugins: [tailwindcss()],
     optimizeDeps: {
       exclude: ["@resvg/resvg-wasm"],
     },
--- apps/web/tailwind.config.mjs
+++ apps/web/tailwind.config.mjs
@@ -1,14 +1,5 @@
 import typography from "@tailwindcss/typography";
 
-const withOpacity = (variableName) => {
-  return ({ opacityValue }) => {
-    if (opacityValue !== undefined) {
-      return `hsl(var(${variableName}) / ${opacityValue})`;
-    }
-    return `hsl(var(${variableName}))`;
-  };
-};
-
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: ["selector", "[data-theme='dark']"],
@@ -21,50 +12,9 @@ export default {
     },
 
     extend: {
-      textColor: {
-        skin: {
-          base: withOpacity("--color-fg"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-bg"),
-        },
-      },
-      backgroundColor: {
-        skin: {
-          fill: withOpacity("--color-bg"),
-          accent: withOpacity("--color-accent"),
-          inverted: withOpacity("--color-fg"),
-          surface: withOpacity("--color-surface"),
-          muted: withOpacity("--color-muted"),
-        },
-      },
-      outlineColor: {
-        skin: {
-          fill: withOpacity("--color-accent"),
-        },
-      },
-      borderColor: {
-        skin: {
-          line: withOpacity("--color-border"),
-          fill: withOpacity("--color-fg"),
-          accent: withOpacity("--color-accent"),
-        },
-      },
-      fill: {
-        skin: {
-          base: withOpacity("--color-fg"),
-          accent: withOpacity("--color-accent"),
-        },
-        transparent: "transparent",
-      },
-      stroke: {
-        skin: {
-          accent: withOpacity("--color-accent"),
-        },
-      },
       fontFamily: {
         mono: ["IBM Plex Mono", "monospace"],
       },
-
       typography: {
         DEFAULT: {
           css: {
